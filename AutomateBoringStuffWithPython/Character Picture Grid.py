1004d59845c0c83edc51f5eff729e764643392ca
asidar <63718466+asidar@users.noreply.github.com>
2020-05-03 17:35:13 +0530

    Update Character Picture Grid.py

--- AutomateBoringStuffWithPython/Character Picture Grid.py
+++ AutomateBoringStuffWithPython/Character Picture Grid.py
@@ -1,1 +1,24 @@
 
+# Below is given List of lists. the code turns the given grid like this [Question is taken from Automate Boring Stuff with Python book]
+#  ..OO.OO..
+#  .OOOOOOO. 
+#  .OOOOOOO. 
+#  ..OOOOO.. 
+#  ...OOO... 
+#  ....O....
+#------------------------------------------------------------------------
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+str=""
+for i in range(0,6):
+    for j in range(0,9):
+        str = str + grid[j][i]
+    print(str)
+    str= ""
