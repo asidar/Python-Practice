a8a1da1f04582968697ab6d923c05e6e42864d48
asidar <63718466+asidar@users.noreply.github.com>
2020-05-03 17:33:51 +0530

    Delete 001.py

--- 001.py
+++
@@ -1,9 +0,0 @@
-# Project Euler Problem 1 
-# Find the sum of all multiples of 3 and 5 below 1000.
-
-sum=0
-for i in range(1,1000):
-    if (i%3==0 or i%5==0):
-        sum = sum + i
-        #print(i)
-print(sum)
