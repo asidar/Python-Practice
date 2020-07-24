e40aa1e0ac26cbc37578f890621d1efb4ea11174
asidar <63718466+asidar@users.noreply.github.com>
2020-07-24 18:52:06 +0530

    Update Count-Words.py
    Counting words in a given text using logic of number of spaces in the text.

--- Count-Words.py
+++ Count-Words.py
@@ -1,6 +1,6 @@
 text="Hello! This is a sample text."
 wnum = 1
-  for i in temp:
-    if (temp == " "):
-      wnum += 1
-print("Number of words in sample text were: {}", wnum)
+for i in text:
+    if (i == " "):
+        wnum += 1
+print("Number of words in sample text were: ", wnum)
