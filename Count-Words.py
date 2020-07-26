fd82e17ce98da4a0323497b07de839ba1472709c
asidar <63718466+asidar@users.noreply.github.com>
2020-07-26 19:03:28 +0530

    Update Count-Words.py

--- Count-Words.py
+++ Count-Words.py
@@ -2,8 +2,8 @@ text= "Hello! This is a sample text."
 wnum = 1
 cnum = 0
 for i in text:
-    cnum +=1
+    cnum +=1       # Can be done using: print(f"Number of Characters in sample text were : {len(text)}")
     if (i == " "): # function counting words depending on number of spaces
         wnum += 1
 print("Number of words in sample text were: ", wnum)
-print ("Number of characters in sample text were: ",cnum)     # Can be done using: print(f"Number of Characters in sample text were : {len(text)}")
+print ("Number of characters in sample text were: ",cnum)
