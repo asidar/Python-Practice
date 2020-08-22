f133db2e01c12631b607eac0b5882a74ddbb8a20
pytxt <63718466+pytxt@users.noreply.github.com>
2020-08-22 10:16:30 -0400

    Create collatz-sequence.py

---
+++ AutomateBoringStuffWithPython/collatz-sequence.py
@@ -0,0 +1,13 @@
+# A function that generates a Collatz Sequence written in Python
+
+def collatz(num):
+    while(num!=1):
+        if (num % 2)==0:
+            num=num//2
+            print(num)
+        else:
+            num=3*num+1
+            print(num)
+num = input("Enter a Number: ")
+num=int(num)
+collatz(num)
